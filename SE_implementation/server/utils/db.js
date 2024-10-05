aa010793b50cb920daf55dd5be50088c0c6d80c4
Nirva-Patel <202201071@daiict.ac.in>
2024-10-05 16:15:25 +0530

    Database set up for login page

--- SE_implementation/server/utils/db.js
+++ SE_implementation/server/utils/db.js
@@ -1,1 +1,19 @@
 //DATABASE CONNECTION WILL BE SET UP
+const mongoose = require("mongoose")
+
+const URI = "mongodb+srv://Nirva-Patel:Nirva%401234@cluster0.74lmg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//mongoose.connect(URI)
+
+const connectDb = async()=>{
+    try{
+       await mongoose.connect(URI);
+       console.log("Connected succesfully to DB")
+    }catch(error){
+        console.error("Database Connection Failed")
+        process.exit(1);
+    }
+};
+
+connectDb();
+
+module.exports = connectDb
